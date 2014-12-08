2b6f4d3fc6c6e7531f1aeb9e815fe78bc4737d20
viniciuspalma <vini_palma@outlook.com>
2014-12-08 13:52:38 -0200

    change some methods in controller

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -9,23 +9,23 @@ module Api
       end
 
       def show
-        respond_with Todo.find params[:id]
+        respond_with assign_todo
       end
 
       def create
         @todo = Todo.new resource_params
         if @todo.save
           respond_to do |format|
-            format.json { render :json => @todo }
+            format.json { render json: @todo }
           end
         end
       end
 
       def update
-        @todo = Todo.find params[:id]
+        @todo = assign_todo
         if @todo.update resource_params
           respond_to do |format|
-            format.json { render :json => @todo }
+            format.json { render json: @todo }
           end
         end
       end
@@ -38,6 +38,10 @@ module Api
 
       def resource_params
         params.require(:todo).permit(:title, :completed)
+      end
+
+      def assign_todo
+        Todo.find params[:id]
       end
 
     end
